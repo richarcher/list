660a0d9b978d0e06a6a6cc3c7eaf1bd68b8c39f1
Rich Archer <rich@plentify.co.za>
2026-03-21 05:47:31 +0200

    chunkload error handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { installChunkLoadRecovery } from './lib/chunkLoadRecovery'
+
+installChunkLoadRecovery()
 
 function setTheme() {
   const dark = window.matchMedia('(prefers-color-scheme: dark)').matches
